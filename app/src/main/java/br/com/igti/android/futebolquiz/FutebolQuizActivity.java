e6d2c793a522da5642e7ee0ec0a682ee411dfd7d
Bruno Mazurok <bruno_mazurok@hotmail.com>
2021-08-02 19:14:08 -0300

    Adicionado a pasta res/raw com os audios, adicionar a class do AudioPlayer, alterado FutebolQuizActivity, adicionado o SingleFragmento

--- app/src/main/java/br/com/igti/android/futebolquiz/FutebolQuizActivity.java
+++ app/src/main/java/br/com/igti/android/futebolquiz/FutebolQuizActivity.java
@@ -3,6 +3,7 @@ package br.com.igti.android.futebolquiz;
 import android.animation.Animator;
 import android.app.Activity;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.Menu;
@@ -20,6 +21,7 @@ public class FutebolQuizActivity extends Activity {
     private static final String TAG = "FutebolQuizActivity";
 
     private static final String KEY_INDICE = "indice";
+    private static final String PREF_PRIMEIRA_VEZ = "primeiraVez";
 
     private Button mBotaoVerdade;
     private Button mBotaoFalso;
@@ -128,6 +130,18 @@ public class FutebolQuizActivity extends Activity {
     protected void onResume() {
         super.onResume();
         Log.d(TAG,"onResume()");
+
+        boolean primeiraVez = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getBoolean(PREF_PRIMEIRA_VEZ,true);
+
+        if (primeiraVez) {
+            PreferenceManager.getDefaultSharedPreferences(getApplicationContext())
+                    .edit()
+                    .putBoolean(PREF_PRIMEIRA_VEZ,false)
+                    .commit();
+            Toast.makeText(this, "Bem-vindo ao FutebolQuiz!", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 
     @Override
